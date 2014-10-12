a2d21abf730d569cd5dad09f77198378f143fab2
MiltonDeng <dch8732@outlook.com>
2014-10-12 22:07:52 +0800

    file #93 bugfix

--- R/rts_process.R
+++ R/rts_process.R
@@ -38,13 +38,16 @@ log <- foreach(i=1:length(rts.files), .combine="rbind" ) %do% {
     select( RIC, Date, Time, Volume)
   setnames(data.oi, old="Volume", new="OI.Volume")
   
+  # debug
+  data.oi <- filter(data.oi, !duplicated(data.oi$Time))
+  
   data.merge <- merge(data.trade, data.quote, 
                       by=c("RIC", "Date", "Time"), all=TRUE) %>>%
     merge( data.oi, by=c("RIC", "Date", "Time"), all=TRUE)
-  
-  message(rts.files[i])
-  message(paste0("i = ", i, ", ", nrow(data.merge), 
-                 " lines of data has been written out."))
+    
+#   message(rts.files[i])
+#   message(paste0("i = ", i, ", ", nrow(data.merge), 
+#                  " lines of data has been written out."))
   
   write.table(data.merge, "data/Reuters/Reuters.csv", sep=",",
             row.names=FALSE, col.names=FALSE, append=TRUE)
@@ -53,6 +56,8 @@ log <- foreach(i=1:length(rts.files), .combine="rbind" ) %do% {
   return(log.i)
 }
 
+log <- data.frame(log)
+names(log) <- c("Files", "nrow", "ncol")
 
 write.csv(log, "data/Reuters/log.csv", row.names=FALSE)
 
@@ -69,3 +74,16 @@ write.csv(log, "data/Reuters/log.csv", row.names=FALSE)
 
 ## Bug
 # task 93 failed - "Join results in 25221 rows; more than 25217 = max(nrow(x),nrow(i)). Check for duplicate key values in i, each of which join to the same group in x over and over again. If that's ok, try including `j` and dropping `by` (by-without-by) so that j runs for each group to avoid the large allocation. If you are sure you wish to proceed, rerun with allow.cartesian=TRUE. Otherwise, please search for this error message in the FAQ, Wiki, Stack Overflow and datatable-help for advice."
+
+## Debug Note 1
+# in file 93, data.oi has two lines with same Time
+# data.oi[ duplicated(data.oi$Time), ]
+# RIC     Date         Time OI.Volume
+# 1: CRSMF5 20141009 10:49:36.494    661720
+# data.oi[ duplicated(data.oi$Time, fromLast=TRUE), ]
+# RIC     Date         Time OI.Volume
+# 1: CRSMF5 20141009 10:49:36.494    661722
+# Here I just omitted one line.
+
+## Debug Note 2
+# file 342 has same problem, but 
