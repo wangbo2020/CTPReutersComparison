9acf65c90f19ed178831c320d174975c56958484
MiltonDeng <dch8732@outlook.com>
2014-10-18 16:43:09 +0800

    RIC id parser

--- R/id_parse.R
+++ R/id_parse.R
@@ -1,5 +1,7 @@
 library(stringr)
 
+## CTP ID Parser --------------------------
+
 id_parse_ctp <- function(id){
   id <- toupper(id)
   del.month <- str_extract(id, "\\d+")
@@ -7,28 +9,47 @@ id_parse_ctp <- function(id){
   
   del.month[ str_length(del.month) == 3 ] <- 
     paste0(1, del.month[ str_length(del.month) == 3 ] )
-           
+  
   StdID <- paste0(product, del.month)     
-
+  
   return(StdID)
 }
 
+## RIC ID Parser ---------------------------------
+
+lookup.table <- read.csv("id_list/RICs.csv", stringsAsFactors=FALSE, header=TRUE) %>>%
+  filter( str_length(StdID) != 0 )
+
 id_parse_rts <- function(RIC){
+  
+  pre.suffix <- str_extract(RIC, "[A-Z]+") %>>%
+    str_sub( start=1, end=-2)
+  
+  product <- sapply(pre.suffix, FUN=function(pre){
+    nline <- match(pre, lookup.table$RIC)
+    product <- lookup.table[nline, "StdID"]
+    return(product)
+  })
+  
+  del.year <- str_sub(RIC, -1) %>>%
+    { paste0(1, .) }
+  
+  del.month <- str_sub(RIC, start=-2, end=-2) %>>%
+    sapply( FUN=function(RIC.month.letter){
+    rts.letters <- c("F", "G", "H", "J", "K", "M", 
+                     "N", "Q", "U", "V", "X", "Z")
+    del.month <- match(RIC.month.letter, rts.letters)  
     
+    if( !is.na(del.month) && str_length(del.month) == 1 ) 
+      del.month <- paste0(0, del.month)
+    
+    return(del.month)
+  })
+  
+  del.time <- paste0(del.year, del.month)
+  
+  StdID <- paste0(product, del.time) %>>%
+    "[<-"( is.na(product), NA)
+  
+  return(StdID)
 }
-
-
-# RICs <- unique(rts[, RIC])
-# RICs
-# 
-# ctp_StdID <- id_parse_ctp(ids)
-# 
-# kinds <- unique(str_extract(ctp_StdID, "[A-Z]+"))
-# kinds
-# 
-# RICs
-# 
-# str_extract(RICs, kinds)
-
-
-# http://cn.reuters.com/article/chinaNews/idCNnCN039037220080505
